e589bf85d4c400a44bcc79e3a6c94b75874def2a
Akseli Hulkkonen <akseli.hulkkonen@gmail.com>
2022-10-19 19:44:23 +0300

    added badguys, redstars and gameover

--- dist/src.a2b27638.js
+++ dist/src.a2b27638.js
@@ -117,7 +117,17 @@ parcelRequire = (function (modules, cache, entry, globalName) {
   }
 
   return newRequire;
-})({"src/index.js":[function(require,module,exports) {
+})({"assets/platform.png":[function(require,module,exports) {
+module.exports = "/platform.4293a96e.png";
+},{}],"assets/star.png":[function(require,module,exports) {
+module.exports = "/star.d86a814f.png";
+},{}],"assets/redStar.png":[function(require,module,exports) {
+module.exports = "/redStar.c6680dea.png";
+},{}],"assets/badGuy.png":[function(require,module,exports) {
+module.exports = "/badGuy.12dd15b5.png";
+},{}],"assets/dude.png":[function(require,module,exports) {
+module.exports = "/dude.83d5008b.png";
+},{}],"src/index.js":[function(require,module,exports) {
 function _typeof(obj) { "@babel/helpers - typeof"; return _typeof = "function" == typeof Symbol && "symbol" == typeof Symbol.iterator ? function (obj) { return typeof obj; } : function (obj) { return obj && "function" == typeof Symbol && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }, _typeof(obj); }
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
@@ -142,7 +152,7 @@ function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.g
 
 var game;
 var gameOptions = {
-  dudeGravity: 500,
+  dudeGravity: 700,
   dudeSpeed: 500
 };
 
@@ -178,17 +188,23 @@ var PlayGame = /*#__PURE__*/function (_Phaser$Scene) {
   var _super = _createSuper(PlayGame);
 
   function PlayGame() {
+    var _this;
+
     _classCallCheck(this, PlayGame);
 
-    return _super.call(this, "PlayGame");
+    _this = _super.call(this, "PlayGame");
+    _this.score = 0;
+    return _this;
   }
 
   _createClass(PlayGame, [{
     key: "preload",
     value: function preload() {
-      this.load.image("ground", "assetsplatform.png");
-      this.load.image("star", "assets/star.png");
-      this.load.spritesheet("dude", "assets/dude.png", {
+      this.load.image("ground", require("../assets/platform.png"));
+      this.load.image("star", require("../assets/star.png"));
+      this.load.image("redStar", require("../assets/redStar.png"));
+      this.load.image("badGuy", require("../assets/badGuy.png"));
+      this.load.spritesheet("dude", require("../assets/dude.png"), {
         frameWidth: 32,
         frameHeigth: 48
       });
@@ -203,34 +219,129 @@ var PlayGame = /*#__PURE__*/function (_Phaser$Scene) {
 
       for (var i = 1; i < 20; i++) {
         this.groundGroup.create(Phaser.Math.Between(0, game.config.width), Phaser.Math.Between(0, game.config.height), "ground");
-      }
+      } //Dude
+
 
       this.dude = this.physics.add.sprite(game.config.width / 2, game.config.height / 2, "dude");
       this.dude.body.gravity.y = gameOptions.dudeGravity;
-      this.physics.add.collider(this.dude, this.groundGroup);
-      this.cursors = this.input.keyboard.createCursorKeys();
+      this.physics.add.collider(this.dude, this.groundGroup); //Stars
+
+      this.starsGroup = this.physics.add.group({});
+      this.physics.add.collider(this.starsGroup, this.groundGroup);
+      this.physics.add.overlap(this.dude, this.starsGroup, this.collectStar, null, this); //Redstars
+
+      this.redStarsGroup = this.physics.add.group({});
+      this.physics.add.collider(this.redStarsGroup, this.groundGroup);
+      this.physics.add.overlap(this.dude, this.redStarsGroup, this.collectRedStar, null, this); //BadGuy
+
+      this.enemiesGroup = this.physics.add.group({});
+      this.physics.add.collider(this.enemiesGroup, this.groundGroup);
+      this.physics.add.collider(this.dude, this.enemiesGroup, this.hitEnemy, null, this); //Scoreboard
+
+      this.add.image(16, 16, "star");
+      this.scoreText = this.add.text(32, 0, "0", {
+        fontsize: "35px",
+        fill: "#ffffff"
+      }); //GameOver
+
+      this.gameOverText = this.add.text(600, 400, "Game Over", {
+        fontsize: "500px",
+        fill: "#000000"
+      });
+      this.gameOverText.setOrigin(0.5);
+      this.gameOverText.visible = false; //controls
+
+      this.cursors = this.input.keyboard.createCursorKeys(); //Dude walking animations
+
+      this.anims.create({
+        key: "left",
+        frames: this.anims.generateFrameNumbers("dude", {
+          start: 0,
+          end: 4
+        }),
+        frameRate: 10,
+        repeat: -1
+      });
+      this.anims.create({
+        key: "turn",
+        frames: [{
+          key: "dude",
+          frame: 4
+        }],
+        frameRate: 10
+      });
+      this.anims.create({
+        key: "right",
+        frames: this.anims.generateFrameNumbers("dude", {
+          start: 5,
+          end: 9
+        }),
+        frameRate: 10,
+        repeat: -1
+      });
+      this.triggerTimer = this.time.addEvent({
+        callback: this.addGround,
+        callbackScope: this,
+        delay: 700,
+        loop: true
+      });
     }
   }, {
     key: "addGround",
     value: function addGround() {
-      console.log("testing add ground");
       this.groundGroup.create(Phaser.Math.Between(0, game.config.width), 0, "ground");
-      this.groundGroup.setVelocityY(gameOptions.dudeSpeed / 6);
+      this.groundGroup.setVelocityY(gameOptions.dudeSpeed / 8); //Falling stars
+
+      if (Phaser.Math.Between(0, 1)) {
+        this.starsGroup.create(Phaser.Math.Between(0, game.config.width), 0, "star");
+        this.redStarsGroup.create(Phaser.Math.Between(0, game.config.width), 0, "redStar");
+        this.enemiesGroup.create(Phaser.Math.Between(0, game.config.width), 0, "badGuy");
+        this.starsGroup.setVelocityY(gameOptions.dudeSpeed);
+        this.enemiesGroup.setVelocityY(gameOptions.dudeSpeed);
+        this.redStarsGroup.setVelocityY(gameOptions.dudeSpeed);
+      }
+    }
+  }, {
+    key: "collectStar",
+    value: function collectStar(dude, star) {
+      star.disableBody(true, true);
+      this.score += 1;
+      this.scoreText.setText(this.score);
+    }
+  }, {
+    key: "collectRedStar",
+    value: function collectRedStar(dude, redStar) {
+      redStar.disableBody(true, true);
+      this.score += 5;
+      this.scoreText.setText(this.score);
+    }
+  }, {
+    key: "hitEnemy",
+    value: function hitEnemy(dude, badGuy) {
+      console.log("gameOver");
+      this.physics.pause();
+      gameOver = true;
+      this.gameOverText.visible = true;
     }
   }, {
     key: "update",
     value: function update() {
+      //Controls
       if (this.cursors.left.isDown) {
         this.dude.body.velocity.x = -gameOptions.dudeSpeed;
+        this.dude.anims.play("left", true);
       } else if (this.cursors.right.isDown) {
         this.dude.body.velocity.x = gameOptions.dudeSpeed;
+        this.dude.anims.play("right", true);
       } else {
         this.dude.body.velocity.x = 0;
+        this.dude.anims.play("turn", true);
       }
 
       if (this.cursors.up.isDown && this.dude.body.touching.down) {
         this.dude.body.velocity.y = -gameOptions.dudeGravity / 1.6;
-      }
+      } //If die, restart
+
 
       if (this.dude.y > game.config.height || this.dude.y < 0) {
         this.scene.start("PlayGame");
@@ -240,7 +351,7 @@ var PlayGame = /*#__PURE__*/function (_Phaser$Scene) {
 
   return PlayGame;
 }(Phaser.Scene);
-},{}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+},{"../assets/platform.png":"assets/platform.png","../assets/star.png":"assets/star.png","../assets/redStar.png":"assets/redStar.png","../assets/badGuy.png":"assets/badGuy.png","../assets/dude.png":"assets/dude.png"}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
@@ -268,7 +379,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "53004" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "58246" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 let game;
 
 const gameOptions = {
-	dudeGravity: 500,
+	dudeGravity: 700,
 	dudeSpeed: 500
 }
 window.onload = function() {
@@ -32,13 +32,18 @@ class PlayGame extends Phaser.Scene {
 
 	constructor() {
 		super("PlayGame")
-
+		this.score = 0
+		
 	}
 	
 	preload(){
-		this.load.image("ground","assetsplatform.png")
-		this.load.image("star", "assets/star.png")
-		this.load.spritesheet("dude","assets/dude.png", {frameWidth: 32, frameHeigth: 48});
+		
+		
+		this.load.image("ground", require("../assets/platform.png"))
+		this.load.image("star", require("../assets/star.png"))
+		this.load.image("redStar", require("../assets/redStar.png"))
+		this.load.image("badGuy", require("../assets/badGuy.png"))
+		this.load.spritesheet("dude",require("../assets/dude.png"), {frameWidth: 32, frameHeigth: 48});
 	}
 
 	create(){
@@ -51,38 +56,129 @@ class PlayGame extends Phaser.Scene {
 			this.groundGroup.create(Phaser.Math.Between(0, game.config.width), Phaser.Math.Between(0, game.config.height), "ground");
 
 		}
+		//Dude
 		this.dude = this.physics.add.sprite(game.config.width / 2, game.config.height / 2, "dude")
 		this.dude.body.gravity.y = gameOptions.dudeGravity
 		this.physics.add.collider(this.dude, this.groundGroup)
 		
+		//Stars
+		this.starsGroup = this.physics.add.group({})
+		this.physics.add.collider(this.starsGroup, this.groundGroup)
+		this.physics.add.overlap(this.dude, this.starsGroup, this.collectStar, null , this)
+		
+		//Redstars
+		this.redStarsGroup = this.physics.add.group({})
+		this.physics.add.collider(this.redStarsGroup, this.groundGroup)
+		this.physics.add.overlap(this.dude, this.redStarsGroup, this.collectRedStar, null , this)
+		
+		//BadGuy
+		this.enemiesGroup = this.physics.add.group({})
+		this.physics.add.collider(this.enemiesGroup, this.groundGroup)
+		this.physics.add.collider(this.dude, this.enemiesGroup, this.hitEnemy, null, this)
+		
+		//Scoreboard
+		this.add.image(16,16, "star")
+		this.scoreText = this.add.text(32,0,"0", {fontsize: "35px", fill: "#ffffff"})
+		
+		//GameOver
+		this.gameOverText = this.add.text(600,400,"Game Over", {fontsize: "500px", fill: "#000000"})
+		this.gameOverText.setOrigin(0.5)
+		this.gameOverText.visible = false
+		
+		//controls
 		this.cursors = this.input.keyboard.createCursorKeys()
+
+		//Dude walking animations
+		this.anims.create({
+			key: "left",
+			frames: this.anims.generateFrameNumbers("dude",{start:0,end: 4}),
+			frameRate: 10,
+			repeat: -1
+		})
+		this.anims.create({
+			key: "turn",
+			frames: [{key: "dude", frame: 4}],
+			frameRate: 10,
+			
+		})
+
+		this.anims.create({
+			key: "right",
+			frames: this.anims.generateFrameNumbers("dude",{start:5,end: 9,}),
+			frameRate: 10,
+			repeat: -1
+		})
+
+		this.triggerTimer = this.time.addEvent({
+			callback: this.addGround,
+			callbackScope: this,
+			delay: 700,
+			loop: true
+		})
 	}
 
+
 	addGround(){
-		console.log("testing add ground")
-		this.groundGroup.create(Phaser.Math.Between(0,game.config.width), 0, "ground")
-		this.groundGroup.setVelocityY(gameOptions.dudeSpeed / 6)
-	
 		
-	
+		this.groundGroup.create(Phaser.Math.Between(0,game.config.width), 0, "ground")
+		this.groundGroup.setVelocityY(gameOptions.dudeSpeed / 8)
+		
+		//Falling stars
+		if(Phaser.Math.Between(0, 1)) {
+            this.starsGroup.create(Phaser.Math.Between(0, game.config.width), 0, "star")
+			this.redStarsGroup.create(Phaser.Math.Between(0, game.config.width), 0, "redStar")
+			this.enemiesGroup.create(Phaser.Math.Between(0, game.config.width),0, "badGuy")
+            this.starsGroup.setVelocityY(gameOptions.dudeSpeed)
+			this.enemiesGroup.setVelocityY(gameOptions.dudeSpeed)
+			this.redStarsGroup.setVelocityY(gameOptions.dudeSpeed)
+        }
+			
+
+	}
+	collectStar(dude, star) {
+		star.disableBody(true, true)
+		this.score += 1
+		this.scoreText.setText(this.score)
+	}
+	collectRedStar(dude, redStar) {
+		redStar.disableBody(true, true)
+		this.score += 5
+		this.scoreText.setText(this.score)
+	}
+	hitEnemy(dude, badGuy){
+		console.log("gameOver")
+		this.physics.pause();
+		gameOver = true;
+		this.gameOverText.visible = true
+		
 	}
 	
 	update() {
+		//Controls
 		if(this.cursors.left.isDown){
 			this.dude.body.velocity.x = -gameOptions.dudeSpeed
+			this.dude.anims.play("left",true)
 		}
 		else if(this.cursors.right.isDown){
 			this.dude.body.velocity.x = gameOptions.dudeSpeed
+			this.dude.anims.play("right",true)
 		}
 		else{
 			this.dude.body.velocity.x = 0
+			this.dude.anims.play("turn",true)
 		}
 		if(this.cursors.up.isDown && this.dude.body.touching.down){
+			
 			this.dude.body.velocity.y = -gameOptions.dudeGravity / 1.6
+
 		}
+		//If die, restart
 		if(this.dude.y > game.config.height || this.dude.y < 0){
 			this.scene.start("PlayGame")
 
 		}
+
+
 	}
+
 }
